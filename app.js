edb0c7878e6474dcf2762c786fd21d3aa9016d00
puntsgaa <tpuntsgaa@gmail.com>
2020-12-02 01:28:26 -0800

    second_commit

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 var uiController = (function () {
-
+    console.log("uiController");
 })();
+
+
+var financeController = (function () {
+    console.log("Finance controller");
+})();
+
+var appController = (function (uiController, financeController) {
+    document.querySelector(".add__btn").addEventListener('click', function () {
+        ctrlAddItem();
+    });
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+    function ctrlAddItem() {
+        var amount = document.querySelector(".add__value").value;
+        console.log(amount);
+    }
+
+})(uiController, financeController);
