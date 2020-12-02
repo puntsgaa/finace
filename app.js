8544506d2c03873cceea3afedb2a177042e0894a
puntsgaa <tpuntsgaa@gmail.com>
2020-12-02 03:34:26 -0800

    commit

--- app.js
+++ app.js
@@ -1,26 +1,130 @@
 var uiController = (function () {
-    console.log("uiController");
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDesc: ".add__description",
+        inputAddValue: ".add__value",
+        btnAdd: ".add__btn"
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                amount: document.querySelector(DOMstrings.inputAddValue).value,
+                description: document.querySelector(DOMstrings.inputDesc).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        },
+        addList: function (item, type) {
+            var html;
+            if (type === 'inc') {
+                list = '.income__list';
+                html = '<div class="item clearfix" id="%id%">' +
+                    '<div class="item__description" >%desc%</div>' +
+                    '<div class="right clearfix">' +
+                    '<div class="item__value">+ %amount%</div>' +
+                    '<div class="item__delete">' +
+                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+            }
+            else {
+                list = '.expenses__list';
+                html = '<div class="item clearfix" id="%id%">' +
+                    '<div class="item__description">%desc%</div>' +
+                    '<div class="right clearfix">' +
+                    '<div class="item__value">- %amount%</div>' +
+                    '<div class="item__percentage">21%</div>' +
+                    '<div class="item__delete">' +
+                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+            }
+            html = html.replace('%id%', item.id);
+            html = html.replace('%desc%', item.description);
+            html = html.replace('%amount%', item.value);
+            document.querySelector(list).insertAdjacentHTML('afterbegin', html);
+        }
+    };
 })();
 
 
 var financeController = (function () {
-    console.log("Finance controller");
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    var data = {
+        allItem: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    }
+
+    return {
+        addItem: function (type, desc, value) {
+            var obj, id;
+            if (data.allItem[type].length === 0) {
+                id = 1;
+            }
+            else {
+                var id = data.allItem[type][data.allItem[type].length - 1].id + 1;
+            }
+            if (type === 'inc') {
+                obj = new Income(id, desc, value);
+            }
+            else {
+                obj = new Expense(id, desc, value);
+            }
+            data.allItem[type].push(obj);
+            console.log(data.allItem)
+            return obj;
+        }
+    }
 })();
 
 var appController = (function (uiController, financeController) {
-    document.querySelector(".add__btn").addEventListener('click', function () {
-        ctrlAddItem();
-    });
+    var ctrlAddItem = function () {
+        var input = uiController.getInput();
+        var addItem = financeController.addItem(input.type, input.description, input.amount);
+        var addHtml = uiController.addList(addItem, input.type);
+    }
 
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
+    var setupEventListeners = function () {
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.btnAdd).addEventListener('click', function () {
             ctrlAddItem();
-        }
-    });
+        });
 
-    function ctrlAddItem() {
-        var amount = document.querySelector(".add__value").value;
-        console.log(amount);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+    return {
+        init: function () {
+            setupEventListeners();
+        }
     }
 
 })(uiController, financeController);
+
+
+appController.init();
